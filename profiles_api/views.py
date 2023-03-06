65fe318c0615ba717e6bf51184b7fac0dbf02c10
y00822511 <cansizyusuf29@gmail.com>
2023-03-06 16:36:08 +0300

    viewset class 2

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -54,6 +54,7 @@ class HelloApiView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
         """"Test API Viewset"""
+        serializers_class = serializers.HElloSerializer
 
         def list(self, request):
                 """Return Hello Message"""
@@ -64,3 +65,30 @@ class HelloViewSet(viewsets.ViewSet):
                 ]
 
                 return Response({'message': 'HEllo!', 'a_viewset': a_viewset})
+        
+        def create(self, request):
+                """Create a new hello message"""
+                serializer = self.serializers_class(data=request.data)
+
+                if serializer.is_valid():
+                        name = serializer.validated_data.get('name')
+                        message = f'Hello {name}!'
+                        return Response({'message': message})
+                else:
+                        return Response(
+                                serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST
+                        )
+                
+        def retrieve(self, request, pk=None):
+                """Handle getting an object by its ID"""
+                return Response({'http_method': 'GET'})
+        
+        def update(self, request, pk=None):
+                return Response({'http_method': 'PUT'})
+        
+        def partial_update(self, request, pk=None):
+                return Response({'http_method': 'PATCH'})
+        
+        def destrou(self, request, pk=None):
+                return Response({'http_method': 'DELETE'})
